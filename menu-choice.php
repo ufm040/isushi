5aabeca6f3f151e1db482cab2c60941147928fd1
Arturas56000 <arthur.sollet@gmail.com>
2015-12-03 14:00:48 +0100

    change arthur

---
+++ menu-choice.php
@@ -0,0 +1,36 @@
+
+ <?php 
+try {
+	$pdo = include('data/pdo.php');
+
+} catch (Exception $e) {
+
+
+        die('Erreur');
+
+}
+		
+		$getimage = $pdo->query('SELECT image, name, description, price  FROM produits');
+		$donnees = $getimage->fetchall();
+ 		
+
+		foreach ($donnees as $key => $value) {
+			?>
+			<button type="submit" value="text" >selectionner</button>
+			<input type="number" name="howmuch">
+			<img src="<?php echo $donnees[$key]['image'];?>"/>
+			<h3><?php echo $donnees[$key]['name'];?></h3>
+			<p><?php echo $donnees [$key]['description'];?></p>
+			<p><?php echo $donnees [$key]['price'] . "€";?></p>
+			
+			
+
+
+
+			<?php
+		}	
+		
+		
+
+	
+?>
