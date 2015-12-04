50d306e906479c3e104237323444bc18244ce330
ufm040 <cmoinon@copar-info.fr>
2015-12-04 15:40:44 +0100

    go 15h40

--- liste-menu.php
+++ liste-menu.php
@@ -1,18 +1,14 @@
  <?php
-if (isset($_SESSION['auth'])) {
-	$connect = "Déconnexion";
-	$connectPage = "deconnexion.php";
-	$nameUser = $_SESSION['auth']['firstname'];
-} else {
-	session_start();
-	$connect = "Connexion";
-	$connectPage = "connexion.php";
-}
-
 
 include('includes/header.php') ;  
-include('product.php');	
-		
+?>
+
+<div id="fullcarte">
+	<?php
+	include('menu-choice.php');	
+	?> 
+</div>
+<?php 		
 include('includes/footer.html') ; 	
 ?>
 
