6cd5cb9abce84f33bfca1c1b43eddf0788956661
2philgit <philippe.agm@gmail.com>
2015-12-04 12:59:49 +0100

    update branche phil / cath
    fichier initproduct.php

--- libs/initproduct.php
+++ libs/initproduct.php
@@ -2,9 +2,11 @@
 	session_start(); 
 	//unset($_SESSION['basket']);
 	$productQty = 0;
-	if (! isset($_SESSION['basket']) ) {
-	 	$_SESSION['basket'][0]= $_POST;
-	 	$productQty += $_POST['qty'];
+	if (! isset($_SESSION['basket'] ) ){
+		if (! isset($_POST) ) {
+			$_SESSION['basket'][0]= $_POST;	
+			$productQty += $_POST['qty'];
+		}
 	} else {
 		$panier = $_SESSION['basket'];		
 		foreach($panier as $key => $value) {
