3e1fed8bff72879fc78ef752ad6b201b64216f18
2philgit <philippe.agm@gmail.com>
2015-12-04 09:21:17 +0100

    Update branche phil vendredi 9h
    modif page connexion.php, inscription.php
    ajout style_phil.css, password.php et newpassword.php

--- inscription.php
+++ inscription.php
@@ -1,29 +1,39 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-	<meta charset="UTF-8">
-	<title>connexion</title>
-</head>
 
 <?php
 
 $error = '';
 $address = '';
 $allFields = !empty($_POST['new_client']['name']) && !empty($_POST['new_client']['firstname']) && !empty($_POST['new_client']['email']) && !empty($_POST['new_client']['password'])&& !empty($_POST['new_client']['password_conf']) && !empty($_POST['new_client']['phone']) && !empty($_POST['new_client']['number_street']) && !empty($_POST['new_client']['street']) && !empty($_POST['new_client']['zip']) && !empty($_POST['new_client']['city']);
+
+// $tab_error = [];
+
+// if ( $_POST ) {
+
+//     $keys = ["name", "email", "message"];
+//     foreach ( $keys as $key ) {
+//         if ( empty($_POST[$key]) || !trim($_POST[$key]) ) {  // trim() supprime les espaces avt et après ds une chaîne
+//                                                              // ici, gère le fait que l'utilisateur est mis des espaces uniquement
+//             // si le champ est vide, on crée l'index correspondant à
+//             // ce champ dans le tableau $error, de manière à le réutiliser plus bas.
+//             $error[$key] = "le champ $key est manquant !\n";
+//         }
+//     }
+
+// }
+
+
+
+
 if ( $_POST ) {
 
-	if ( !empty($_POST['action']['registred']) ) {
+if (empty($_POST['action']['connexion'])) { // test en premier si le bouton "connexion" n'a pas été cliqué
+
+	if ( !empty($_POST['action']['registred'])) {
 		
 		if ($allFields) {
-		// if ( !empty($_POST['new_client']['name']) && !empty($_POST['new_client']['firstname']) 
-		// 	&& !empty($_POST['new_client']['email']) && !empty($_POST['new_client']['password'])
-		// 	&& !empty($_POST['new_client']['password_conf']) && !empty($_POST['new_client']['phone'])
-		// 	&& !empty($_POST['new_client']['number_street']) && !empty($_POST['new_client']['street']) 
-		// 	&& !empty($_POST['new_client']['zip']) && !empty($_POST['new_client']['city']) ) {
 
 			$pdo = include('data/pdo.php');
 			$address = $_POST['new_client']['number_street'] . ", " . $_POST['new_client']['street'] . ", " . $_POST['new_client']['zip'] . " " . $_POST['new_client']['city'];
-			// echo($_POST['new_client']['firstname']);
 
 			$statement = $pdo->prepare('INSERT INTO clients (`name`, `firstname`, `email`, `password`, `phone`, `address`, `created`) 
 										VALUES (:name, :firstname, :email, :password, :phone, :address, NOW()) ;');
@@ -35,15 +45,20 @@ if ( $_POST ) {
 				':phone' => $_POST['new_client']['phone'],
 				':address' => $address,
 			]);
-			//$users = $statement->fetchAll();
-			die("formulaire validé !");
+
+			if ( $_POST['new_client']['password'] === $_POST['new_client']['password_conf'] ) {
+
+			$statement = $pdo->prepare('SELECT * FROM clients WHERE email = ? ;');
+			$statement->execute([$_POST['new_client']['email']]);
+			$users = $statement->fetchAll();
+	
 			// if ( count($users) ) {
 				
-			// 	session_start();
-			// 	$_SESSION['auth'] = $users[0];
-			// 	//die(header('Location: index.html'));
-			// 	die(header('Location: ./' . ( !empty($_POST['action']['next']) ? $_POST['action']['next'] : '' ) ));
-			// 	// die("vous êtes connecté");
+				session_start();
+				$_SESSION['auth'] = $users[0];
+				die(header('Location: ./' . ( !empty($_POST['action']['next']) ? $_POST['action']['next'] : '' ) ));
+
+			} else $error = "Les mots de passes ne sont pas identiques.";
 
 			// } else $error = "Email ou mot de passe erroné.";
 
@@ -52,22 +67,28 @@ if ( $_POST ) {
 	} else $error = "Le formulaire n'a pas été correctement validé.";
 
 }
-
+}
 
 ?>
 
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<title>inscription</title>
+</head>
 <body>
 	<h2>Nouveau client</h2>
 	<h3>Formulaire d'inscritpion</h3>
 
-	<form id="form-new-client" action="inscription.php" method="POST">
+	<form id="form-new-client" action="connexion.php" method="POST">
 
 		<?php if ( $error ) { ?>
 			<h2 style="color: red;"><?=$error?></h2>
 		<?php } ?>
 
 		<fieldset>
-
+			<!-- <label class="<?=!empty($error['new_client']['name']) ? 'error' : '' ?>">nom :</label><br /> -->
 			<input
 				type="text"
 				placeholder="nom"
